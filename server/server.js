f948ebbd786ce6459ce5ba5b4f75790e9b3fd57f
divyaks30 <ksdivya2004@gmail.com>
2026-06-01 23:03:20 +0530

    Fix CORS for Vercel frontend

--- server/server.js
+++ server/server.js
@@ -12,7 +12,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mern-todo-app-red.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
